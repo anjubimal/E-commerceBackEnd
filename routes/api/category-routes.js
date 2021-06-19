4ae85c530542c9d2d19cd4c9f3b4040da4040a8d
anjubimal <anjubimal@tamu.edu>
2021-06-19 17:16:27 -0400

    modified models and routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,8 +21,24 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
+
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
